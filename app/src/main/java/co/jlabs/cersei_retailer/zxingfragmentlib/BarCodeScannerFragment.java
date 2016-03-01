125638af3c83f484298466680dd27c2ff23722a3
kashyapsharma <kashyap.sharma@jlabs.co>
2016-03-01 14:50:18 +0530

    inbuilt round progress.

--- app/src/main/java/co/jlabs/cersei_retailer/zxingfragmentlib/BarCodeScannerFragment.java
+++ app/src/main/java/co/jlabs/cersei_retailer/zxingfragmentlib/BarCodeScannerFragment.java
@@ -17,8 +17,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.DecodeHintType;
@@ -31,31 +30,10 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
-/**
- * This activity opens the camera and does the actual scanning on a background thread. It draws a
- * viewfinder to help the user place the barcode correctly, shows feedback as the image processing
- * is happening, and then overlays the results when a scan is successful.
- *
- * @author dswitkin@google.com (Daniel Switkin)
- * @author Sean Owen
- */
+
+
 public class BarCodeScannerFragment extends Fragment implements SurfaceHolder.Callback, IConstants {
 
     private static final String TAG = BarCodeScannerFragment.class.getSimpleName();
